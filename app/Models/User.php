ab66459bdd5a75ffaad58d5be0f7e94ce81a5312
xujin <2672070036@qq.com>
2018-12-06 02:40:09 +0000

    Add account activations

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,6 +26,16 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
+
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
